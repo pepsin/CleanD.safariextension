8f9f3ce7c1ea803a21ce5f6fbc1ff3c866dfda2e
pepsin <pepsin@me.com>
2013-02-15 09:11:21 +0800

    Temp Modified For Highlighted User, Not Done, Need To Merge 3 Method to One.

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
+
+
 $(function() {
-  var getCAnswer, getCWord, getHAnswer, getHWord;
+  var getAnswer, getCAnswer, getCWord, getHAnswer, getHWord, getWord;
+  getWord = function(theData) {
+    return safari.self.tab.dispatchMessage("getWord", theData);
+  };
+  getAnswer = function(theMessageEvent) {
+    var cword_str, cwords;
+    if (theMessageEvent.name === "theCAnswer") {
+      cword_str = theMessageEvent.message;
+      cwords = cword_str.split(',');
+      console.log(cwords);
+      return $('td.author cite a').each(function() {
+        var cword, ele, h, reg, _i, _len, _results;
+        ele = $(this);
+        _results = [];
+        for (_i = 0, _len = cwords.length; _i < _len; _i++) {
+          cword = cwords[_i];
+          if (cword.length !== 0) {
+            reg = RegExp(cword, 'i');
+            if (ele.html().match(reg)) {
+              h = ele.parents('tbody');
+              _results.push(h.css('background-color', '#fca'));
+            } else {
+              _results.push(void 0);
+            }
+          } else {
+            _results.push(void 0);
+          }
+        }
+        return _results;
+      });
+    }
+  };
+  safari.self.addEventListener("message", getAnswer, false);
+  getWord("");
   getCWord = function(theData) {
     return safari.self.tab.dispatchMessage("getCWord", theData);
   };
@@ -39,7 +74,6 @@ $(function() {
     var hword_str, hwords;
     if (theMessageEvent.name === "theHAnswer") {
       hword_str = theMessageEvent.message;
-      console.log(hword_str);
       hwords = hword_str.split(',');
       return $('th.subject span a').each(function() {
         var ele, h, hword, reg, _i, _len, _results;
--- main.js.coffee
+++ main.js.coffee
@@ -1,4 +1,23 @@
 $ ->
+  getWord = (theData) ->
+    safari.self.tab.dispatchMessage "getWord", theData
+  getAnswer = (theMessageEvent) ->
+    if theMessageEvent.name is "theCAnswer"
+      cword_str = theMessageEvent.message
+      cwords = cword_str.split(',')
+      console.log cwords
+      $('td.author cite a').each ->
+        ele = $(@)
+        for cword in cwords
+          if cword.length isnt 0
+            reg = RegExp cword,'i'
+            if ele.html().match reg
+              h = ele.parents('tbody')
+              h.css('background-color', '#fca')
+      
+  safari.self.addEventListener "message", getAnswer, false
+  getWord ""
+  
   getCWord = (theData) ->
     safari.self.tab.dispatchMessage "getCWord", theData
   getCAnswer = (theMessageEvent) ->
@@ -22,7 +41,6 @@ $ ->
   getHAnswer = (theMessageEvent) ->
     if theMessageEvent.name is "theHAnswer"
       hword_str = theMessageEvent.message
-      console.log hword_str
       hwords = hword_str.split(',')
       $('th.subject span a').each ->
         ele = $(@)
